4ed24aa9108fca1d07fb0d5d87918015fe3e7cb5
vinay <vinay@LAPTOP-HI8ALQCP.localdomain>
2026-08-11 07:27:43 +0000

    started medicine service

---
+++ pharmacy_medicine_service/pharmacy_medicine_service/src/test/java/com/epharmacy/pharmacy_medicine_service/PharmacyMedicineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epharmacy.pharmacy_medicine_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyMedicineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
