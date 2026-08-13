fff9e2a54cc51566a5e6ab027174d82d30f22f28
Vinay Acharya <vinay948276@gmail.com>
2026-08-13 10:28:54 +0530

    Delete pharmacy_medicine_service/pharmacy_medicine_service directory

--- pharmacy_medicine_service/pharmacy_medicine_service/src/main/java/com/epharmacy/pharmacy_medicine_service/exception/MedicineNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.epharmacy.pharmacy_medicine_service.exception;
-
-public class MedicineNotFoundException extends RuntimeException {
-	
-	public MedicineNotFoundException(String message) {
-		super(message);
-	}
-
-}
