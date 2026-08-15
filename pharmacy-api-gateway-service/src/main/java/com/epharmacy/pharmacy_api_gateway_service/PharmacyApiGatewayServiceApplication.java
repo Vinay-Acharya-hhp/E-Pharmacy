f80b0f3ce8db5ec1a842e9e824a85f1ae6b24f3d
vinay <vinay@LAPTOP-HI8ALQCP.localdomain>
2026-08-15 08:25:58 +0000

    added api gateway

---
+++ pharmacy-api-gateway-service/src/main/java/com/epharmacy/pharmacy_api_gateway_service/PharmacyApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epharmacy.pharmacy_api_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyApiGatewayServiceApplication.class, args);
+	}
+
+}
